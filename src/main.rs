49bff17191ec81fb129d122931160b7f5ce7130a
zengolutil <zengolutil@gmail.com>
2024-08-16 22:00:51 -0300

    falta resolver la parte de e/s por el usuario

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+struct Materia{
+    asignatura: string;
+    nota: i32;
+}
+//let mut vector = vec![1, 2, 3];
+
+let mut vector = vec![];
+
+loop {
+    println!("Coloque Nombre materia [Solo enter para salir]:");
+    
+
+
+}
+
+fn main() {
+    
+}
